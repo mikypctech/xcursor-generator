9d1d9426bd36c67a7b1f05012ee3ac653619bcb4
mikypctech <mikypctech@gmail.com>
2026-07-15 05:20:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # ---------------------------------------------------------
-# Copyright (c) 2026 [Your Name]. All Rights Reserved.
+# Copyright (c) 2026 mikypctech. All Rights Reserved.
 # This software is proprietary and not for redistribution.
 # ---------------------------------------------------------
 
