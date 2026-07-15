d6138f6a46b5dc0171a5028e0ea0feab9b32eff2
mikypctech <mikypctech@gmail.com>
2026-07-15 05:19:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# ---------------------------------------------------------
+# Copyright (c) 2026 [Your Name]. All Rights Reserved.
+# This software is proprietary and not for redistribution.
+# ---------------------------------------------------------
+
 # main.py
 from xcursor_generator import main
 
